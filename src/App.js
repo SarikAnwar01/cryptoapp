9bf27c5abab22b821c87fcb2af645fb8fc2b1d57
SarikAnwar01 <sarikanwar007@gmail.com>
2022-01-23 00:02:30 +0545

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react router dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import { Navbar, Exchanges, Cryptocurrencies, CryptoDetails, News, Homepage } from './components';
+import './App.css'
 const App = () => {
     return (
         <div className='app'>
@@ -9,10 +10,32 @@ const App = () => {
                 <Navbar />
             </div>
             <div className='main'>
-
-            </div>
-            <div className='footer'>
-
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<Homepage />} />
+                            <Route exact path="/exchanges" element={<Exchanges />} />
+                            <Route exact path="/crpto/:coinId" element={<CryptoDetails />} />
+                            <Route
+                                exact
+                                path="/cryptocurrencies"
+                                element={<Cryptocurrencies />}
+                            />
+                            <Route exact path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className='footer' level={5} >
+                    <Typography.Title style={{ color: 'white', textAlign: 'center' }}>
+                        Crypto App <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     );
